f25233a44a7a9c2a0014e5b7fc1531d398d0b451
christopher floess <skeptikos@gmail.com>
2015-09-21 18:07:00 +0200

    implement plot1 and plot2

---
+++ plot1.R
@@ -0,0 +1,13 @@
+unzip("exdata-data-NEI_data.zip")
+
+scc <- readRDS('Source_Classification_Code.rds')
+nei <- readRDS('summarySCC_PM25.rds')
+
+nei$year <- as.factor(nei$year)
+
+aggregateEmissions <- aggregate(Emissions ~ year, nei, sum)
+
+plot(aggregateEmissions$year, aggregateEmissions$Emissions, xlab = 'year', ylab = 'Emissions')
+
+dev.copy(png, file= 'plot1.png')
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,15 @@
+unzip("exdata-data-NEI_data.zip")
+
+scc <- readRDS('Source_Classification_Code.rds')
+nei <- readRDS('summarySCC_PM25.rds')
+
+nei$year <- as.factor(nei$year)
+
+neiBaltimore <- nei[nei$fips == "24510",]
+
+aggregateEmissions <- aggregate(Emissions ~ year, neiBaltimore, sum)
+
+plot(aggregateEmissions$year, aggregateEmissions$Emissions, xlab = 'year', ylab = 'Emissions')
+
+dev.copy(png, file= 'plot2.png')
+dev.off()
